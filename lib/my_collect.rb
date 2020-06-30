b2a5a34ff197fdfe13492ccd867f88700cbbba61
Dazha Brown <dajahbrown@hotmail.com>
2020-06-30 00:26:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,15 @@
-def hello(array)
+def my_collect(array)
   i = 0
   collection = []
-  while i < array.length
+  while i < array.count
     collection << yield(array[i])
     i += 1
   end
   collection
 end
+
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end 
